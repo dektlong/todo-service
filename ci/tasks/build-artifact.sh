9b612be5a4ba0434a8b3699f9d92cc6fd96b00b3
dektlong <dekel.tankel@gmail.com>
2020-08-23 10:24:39 -0700

    Update build-artifact.sh

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
-
-export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd git-repo
-gradle assemble -Pversion=$version
+mvn clean install
 cp build/libs/*.jar ../artifact-dir/
