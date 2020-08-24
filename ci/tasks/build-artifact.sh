7601f4782a566189afcd4d6fb3339f9fd88e0dee
dektlong <dekel.tankel@gmail.com>
2020-08-23 20:18:29 -0700

    Update build-artifact.sh

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -3,6 +3,8 @@ set -e
 
 cd git-repo
 
-mvn clean install
+#mvn clean install
 
-cp target/dekt-todo-service.jar ../artifact-dir/
+#cp target/dekt-todo-service.jar ../artifact-dir/
+
+cp https://www.dropbox.com/s/2ny2zlacyg0bvb0/dekt-todo-service.jar?dl=0 ../artifact-dir/
