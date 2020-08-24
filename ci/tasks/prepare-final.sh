3a26543e736a81a99f317525c3e7b0a7042f24b1
dektlong <dekel.tankel@gmail.com>
2020-08-23 19:22:21 -0700

    Update prepare-final.sh

--- ci/tasks/prepare-final.sh
+++ ci/tasks/prepare-final.sh
@@ -4,4 +4,4 @@ set -e
 version=`cat version/number`
 
 echo "Renaming release-candidate to final build"
-cp release-candidate/${base_name}*.jar final-dir/${base_name}-${version}.jar
+cp release-candidate/${base_name}*.jar final-dir/${base_name}-final.jar
