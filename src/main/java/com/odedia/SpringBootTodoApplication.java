1d1cbedf111d9119831693d364f6d193a9f762a2
dektlong <dekel.tankel@gmail.com>
2020-09-02 10:43:41 -0700

    Update SpringBootTodoApplication.java

--- src/main/java/com/odedia/SpringBootTodoApplication.java
+++ src/main/java/com/odedia/SpringBootTodoApplication.java
@@ -62,11 +62,7 @@ interface TodoRepository extends JpaRepository<Todo, Long> {
 class TaskEventHandler {
 	@HandleBeforeSave
 	public void handleBeforeSave(Todo todo) {
-		//log.info("Saving todo: {}", todo.getTitle());
-		//if (todo.getCompleted()) {
-		//	log.info("This Todo is completed: {}", todo.getTitle());
-		//}
-		//log.info("Completed saving todo: {}", todo.getTitle());
+		todo.setTitle(todo.getTitle()+"*** now I am blue ***");
 	}
 }
 
